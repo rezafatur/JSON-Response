2edcef650105aa2b93508ff7d090798dfc7fa806
Muhammad Reza Faturrahman <87164482+rezafatur@users.noreply.github.com>
2023-04-30 17:59:07 +0800

    Update compare.go

--- Nomor 2 - Compare Arrays/compare.go
+++ Nomor 2 - Compare Arrays/compare.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
@@ -15,29 +15,29 @@ func main() {
     // Deklarasi variabel array 1 dan array 2
     // Tipe data string dengan panjang sesuai input pengguna
     arr1 := make([]string, length)
-	arr2 := make([]string, length)
+    arr2 := make([]string, length)
 
-	fmt.Println("\nInput:")
+    fmt.Println("\nInput:")
     // Input array 1 
-	fmt.Print("Array 1: ")
+    fmt.Print("Array 1: ")
 
-	// Looping input array 1
+    // Looping input array 1
     for i := 0; i < length; i++ {
-		fmt.Scan(&arr1[i])
+        fmt.Scan(&arr1[i])
     }
 
     // Input array 2
-	fmt.Print("Array 2: ")
+    fmt.Print("Array 2: ")
 
-	// Looping input array 2
+    // Looping input array 2
     for i := 0; i < length; i++ {
-		fmt.Scan(&arr2[i])
+        fmt.Scan(&arr2[i])
     }
 
-	fmt.Println("\nOutput:")
+    fmt.Println("\nOutput:")
     // Looping untuk membandingkan array 1 dan array 2 sesuai indeks
     for i := 0; i < length; i++ {
-		// Jika tidak sama, maka keluarkan hasilnya
+        // Jika tidak sama, maka keluarkan hasilnya
         if arr1[i] != arr2[i] {
             fmt.Printf("Indeks ke %d berbeda\n", i)
         }
